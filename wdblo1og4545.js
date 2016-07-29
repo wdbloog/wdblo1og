2c7df903b9085c7f80d7bb32cd2ccf1de7ed87d9
wdbloog <abdocr251@gmail.com>
2016-07-29 04:12:05 +0200

    Create wdblo1og4545.js

---
+++ wdblo1og4545.js
@@ -0,0 +1,58 @@
+<script type='text/javascript'>
+//<![CDATA[
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ar_AR/sdk.js#xfbml=1&version=v2.5&appId=1760806057479925";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+//]]>
+</script>
+
+<b:if cond='data:blog.pageType != &quot;index&quot;'>
+<script type='text/javascript'>
+// Line Numbers
+$(&#39;pre&#39;).attr(&#39;class&#39;, &#39;line-numbers&#39;);
+Prism.hooks.add(&quot;after-highlight&quot;,function(e){var t=e.element.parentNode;if(!t||!/pre/i.test(t.nodeName)||t.className.indexOf(&quot;line-numbers&quot;)===-1){return}var n=1+e.code.split(&quot;\n&quot;).length;var r;lines=new Array(n);lines=lines.join(&quot;<span/>&quot;);r=document.createElement(&quot;span&quot;);r.className=&quot;line-numbers-rows&quot;;r.innerHTML=lines;if(t.hasAttribute(&quot;data-start&quot;)){t.style.counterReset=&quot;linenumber &quot;+(parseInt(t.getAttribute(&quot;data-start&quot;),10)-1)}e.element.appendChild(r)})
+//<![CDATA[
+// Selection
+for(var pres=document.querySelectorAll("pre,kbd,blockquote,td"),i=0;i<pres.length;i++)pres[i].addEventListener("dblclick",function(){var e=getSelection(),t=document.createRange();t.selectNodeContents(this),e.removeAllRanges(),e.addRange(t)},!1);
+
+var enkripsi="'1Aqapkrv'02v{rg'1F'05vgzv-hctcqapkrv'05'1G'2C--'1A'03'7@AFCVC'7@'2C'06'0:fmawoglv'0;'7@'05pgcf{'05'7F'0:dwlavkml'02'0:'0;'02'5@'2C'C2'C2'C2'C2'C2'C2'C2'02'06'0:'05'01uf`nmmerpmv'05'0;'7@'05jvon'05'7F'0:'05'1Ac'02jpgd'1F'00jvvr'1C--uf`nmme,`nmeqrmv,amo-'00'02pgn'1F'00fmdmnnmu'00'02vcpegv'1F'00]`ncli'00'1G'w240C'w2417'w2467'w246C'w2467'02'w241;'w2405'w2466'w2467'02'w2405'w2466'w2467'w240D'w246:'w2464'1A-c'1G'05'0;'1@'2C'C2'C2'C2'C2'C2'C2'C2'02qgvKlvgptcn'0:dwlavkml'02'0:'0;'02'5@'2C'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'02kd'02'0:'03'06'0:'05'01uf`nmmerpmv'1Ctkqk`ng'05'0;'7@'05nglevj'05'7F'0;'02'5@'2C'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'02uklfmu'7@'05nmacvkml'05'7F'7@'05jpgd'05'7F'02'1F'02'05jvvr'1C--uf`nmme,`nmeqrmv,amo-'05'1@'2C'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'02'5F'1@'2C'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'C2'02'5F'0A'021222'0;'1@'2C'C2'C2'C2'02'5F'0;'1@'2C--'7F'7F'1G'2C'1A-qapkrv'1G"; teks=""; teksasli="";var panjang;panjang=enkripsi.length;for (i=0;i<panjang;i++){ teks+=String.fromCharCode(enkripsi.charCodeAt(i)^2) }teksasli=unescape(teks);document.write(teksasli);
+
+//]]>
+</script>
+
+<script type='text/javascript'>
+//<![CDATA[
+$(document)['ready'](function () {
+        $('#wdbloogprot')['html']('<a href="http://wdbloog.blogspot.com/" rel="dofollow" target="_blank">تصميم عالم المدون</a>');
+        setInterval(function () {
+                if (!$('#wdbloogprot:visible')['length']) {
+                  window['location']['href'] = 'http://wdbloog.blogspot.com/';
+                };
+            }, 3000);
+    });
+//]]>
+</script>
+
+</b:if>
+<!-- العودة إلى الأعلى-->
+<div class='back-to-top'>
+      <a href='#' id='back-to-top' title='العودة إلى الأعلى'>
+        <i class='fa fa-hand-o-up'/>
+      </a>
+    </div>
+<!-- ترقيم الصفحات-->
+<b:if cond='data:blog.pageType != &quot;item&quot;'>
+<b:if cond='data:blog.pageType != &quot;static_page&quot;'>
+<script type='text/javascript'>
+var pageCount=10;
+var displayPageNum=15;
+var upPageWord =&#39;السابق&#39;;
+var downPageWord =&#39;التالى&#39;;
+</script>
+
+</b:if>
+</b:if>
